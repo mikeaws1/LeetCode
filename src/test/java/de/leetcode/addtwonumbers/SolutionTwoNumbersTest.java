d670ce15958482aebc0ac6facabecdf0391e499d
Michael Alberter <michael.alberter@adesso.de>
2022-06-09 14:06:22 +0200

    Test hinzugefügt

--- src/test/java/de/leetcode/addtwonumbers/SolutionTwoNumbersTest.java
+++ src/test/java/de/leetcode/addtwonumbers/SolutionTwoNumbersTest.java
@@ -6,16 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class SolutionTwoNumbersTest {
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     * int val;
-     * ListNode next;
-     * ListNode() {}
-     * ListNode(int val) { this.val = val; }
-     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
 
     private final SolutionTwoNumbers solutionTwoNumbers = new SolutionTwoNumbers();
 
