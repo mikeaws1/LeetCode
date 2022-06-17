77ee440dbec811d4fb854f118a81e14f9c3cba68
Michael Alberter <michael.alberter@outlook.de>
2022-06-17 10:37:20 +0200

    Formatierung geändert

--- src/main/java/de/leetcode/util/Util.java
+++ src/main/java/de/leetcode/util/Util.java
@@ -3,9 +3,11 @@ package de.leetcode.util;
 public class Util {
     
     public static int power(int exp) {
+
        int power = 1;
-       for (int i = 0; i < exp; i++)  {
-            power =  power * 10;
+
+       for (int i = 0; i < exp; i++) {
+            power = power * 10;
        }
 
        return power;
