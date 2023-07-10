5a214cee131425d826cd0d41ac3f80335fedda99
Michael Alberter <michael.alberter@outlook.de>
2023-07-10 21:22:31 +0200

    Implement iterative and recursive solutions

--- src/main/java/de/leetcode/addtwonumbers/SolutionTwoNumbers.java
+++ src/main/java/de/leetcode/addtwonumbers/SolutionTwoNumbers.java
@@ -5,32 +5,109 @@ import de.leetcode.util.ListNode;
 
 public class SolutionTwoNumbers {
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        if (l1 == null && l2 == null) {
-            return null;
-        }
-        if (l1 == null) {
-            return new ListNode(l2.number, addTwoNumbers(null, l2.next));
-        }
-        if (l2 == null) {
-            return new ListNode(l1.number, addTwoNumbers(l1.next, null));
-        }
+    public ListNode addTwoNumbersIterativ(ListNode l1, ListNode l2) {
+        ListNode firstIter;
+        ListNode secondIter;
+        ListNode result = null;
+        ListNode resultList = null;
 
-        final int l = l1.number + l2.number;
-        int rest = l;
-
-        int v = 0;
-        if (l > 9) {
-            rest  = l % 10;
-            v = 1;
+        if (length(l1, l2)) {
+            firstIter = l1;
+            secondIter = l2;
+        } else {
+            firstIter = l2;
+            secondIter = l1;
         }
 
+        int carry = 0;
+        while (firstIter != null) {
 
-        ListNode n = addTwoNumbers(l1.next, l2.next);
-        if (n != null) {
-            n.number += v;
+            int number;
+            if (secondIter != null) {
+                number = firstIter.val + secondIter.val;
+            } else {
+                number = firstIter.val;
+            }
+            number += carry;
+
+            if (result != null) {
+                result.next = new ListNode(number %10);
+                result = result.next;
+            }
+            else {
+                resultList = result = new ListNode(number % 10);
+            }
+
+            carry = 0;
+            if (number >= 10) {
+                carry = 1;
+            }
+
+            firstIter = firstIter.next;
+            if (secondIter != null) {
+                secondIter = secondIter.next;
+            }
         }
-        return new ListNode(rest,  n);
+        if (carry == 1) {
+            result.next = new ListNode(1);
+        }
+
+        return resultList;
     }
+
+    private boolean length(ListNode start1, ListNode start2) {
+        if (start2 == null) {
+            return true;
+        }
+
+        while (start1 != null) {
+            start1 = start1.next;
+            if (start2 == null) {
+                return true;
+            }
+            start2 = start2.next;
+        }
+        if (start2 != null) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return addTwoNumbersRecursive(l1, l2, 0);
+    }
+
+    public ListNode addTwoNumbersRecursive(ListNode l1, ListNode l2, int carry){
+       if (l1 == null && l2 == null)  {
+           if (carry == 1) {
+               return new ListNode(1);
+           }
+           else {
+               return null;
+           }
+       }
+
+       else if (l1 == null ) {
+           int num = l2.val + carry;
+           int c = num >= 10?1:0;
+           return new ListNode(num % 10, addTwoNumbersRecursive(l2.next, null, c));
+       }
+       else if (l2 == null) {
+           int num = l1.val + carry;
+           int c = num >= 10?1:0;
+           return new ListNode(num % 10, addTwoNumbersRecursive(null, l1.next, c));
+
+       }
+
+       int number = l1.val + l2.val + carry;
+       int rest = number % 10;
+
+       ListNode n = addTwoNumbersRecursive(l1.next, l2.next, number >= 10?1:0);
+
+       return new ListNode(rest, n);
+    }
+
+
 }
--- src/test/java/de/leetcode/addtwonumbers/SolutionTwoNumbersTest.java
+++ src/test/java/de/leetcode/addtwonumbers/SolutionTwoNumbersTest.java
@@ -27,6 +27,42 @@ class SolutionTwoNumbersTest {
     }
 
     @Test
+    void testAddNumbers3() {
+
+        final ListNode list1 = Util.createListReverse(new int[]{1, 9, 9});
+        final ListNode list2 = Util.createListReverse(new int[]{1});
+
+        ListNode node = solutionTwoNumbers.addTwoNumbers(list1, list2);
+        int[] result = new int[]{2, 0, 0};
+
+        assertResult(node, result);
+    }
+
+    @Test
+    void testAddNumbers2() {
+
+        final ListNode list1 = Util.createListReverse(new int[]{9, 9, 9});
+        final ListNode list2 = Util.createListReverse(new int[]{9, 9});
+
+        ListNode node = solutionTwoNumbers.addTwoNumbers(list1, list2);
+        int[] result = new int[]{1, 0, 9,8};
+
+        assertResult(node, result);
+    }
+
+    @Test
+    void testAddNumbers_9999() {
+
+        final ListNode list1 = Util.createListReverse(new int[]{9,9,9,9,9,9,9});
+        final ListNode list2 = Util.createListReverse(new int[]{9,9,9,9});
+
+        ListNode node = solutionTwoNumbers.addTwoNumbers(list1, list2);
+        //int[] result = new int[]{8,9,9,9,0,0,0,1};
+        int[] result = new int[]{1, 0, 0, 0, 9, 9, 9, 8};
+        assertResult(node, result);
+    }
+
+    @Test
     public void testAddNumbersUnequalLength() {
 
         final ListNode list1 = Util.createListReverse(new int[]{4, 9, 3});
@@ -34,6 +70,16 @@ class SolutionTwoNumbersTest {
 
         ListNode node = solutionTwoNumbers.addTwoNumbers(list1, list2);
         int[] result = new int[]{8, 2, 9, 2, 1};
+
+        assertResult(node, result);
+    }
+
+    @Test
+    public void testAddNumbersLengthTwo() {
+        final ListNode list1 = Util.createListReverse(new int[]{7, 3});
+        final ListNode list2 = Util.createListReverse(new int[]{2, 9});
+        ListNode node = solutionTwoNumbers.addTwoNumbers(list1, list2);
+        int[] result = new int[]{1, 0, 2};
 
         assertResult(node, result);
     }
