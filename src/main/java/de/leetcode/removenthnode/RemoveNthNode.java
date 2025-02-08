d8c1d14792c8b6a735daab817aaadec1cb7a581d
Michael Alberter <michael.alberter@outlook.de>
2025-02-08 09:14:19 +0100

    Add remove nth node

--- src/main/java/de/leetcode/removenthnode/RemoveNthNode.java
+++ src/main/java/de/leetcode/removenthnode/RemoveNthNode.java
@@ -3,9 +3,32 @@ package de.leetcode.removenthnode;
 import de.leetcode.util.ListNode;
 
 public class RemoveNthNode {
-    
-    public ListNode removeNthNode(ListNode head, int n) {
 
-        return null;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        int length = count(head);
+        ListNode node = head;
+        int l = length - n;
+
+        if (l == 0)
+            return head.next;
+
+        while (l > 1) {
+            node = head.next;
+            l--;
+        }
+
+        if (node.next != null) {
+            node.next = node.next.next;
+        } else {
+            head = null;
+        }
+
+        return head;
+    }
+
+    private int count(ListNode head) {
+        if (head == null) return 0;
+        return 1 + count(head.next);
     }
 }
